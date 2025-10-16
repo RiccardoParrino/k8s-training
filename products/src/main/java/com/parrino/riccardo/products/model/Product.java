1bfede18e922fb6bfeff7d645b36d3260133d799
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-16 09:31:07 +0200

    adding all necessary changes to product-ms

---
+++ products/src/main/java/com/parrino/riccardo/products/model/Product.java
@@ -0,0 +1,34 @@
+package com.parrino.riccardo.products.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Double price;
+
+    private String year;
+
+}
